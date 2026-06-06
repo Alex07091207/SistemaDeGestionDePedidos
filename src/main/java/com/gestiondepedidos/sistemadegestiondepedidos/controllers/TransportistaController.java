b5191ce4f50bfdea3fae7e798d760d6cf7b89d40
Alexander <alexanderdiaz2001@hotmail.com>
2026-06-05 22:59:07 -0400

    Crear metodo Post de transportista

--- src/main/java/com/gestiondepedidos/sistemadegestiondepedidos/controllers/TransportistaController.java
+++ src/main/java/com/gestiondepedidos/sistemadegestiondepedidos/controllers/TransportistaController.java
@@ -3,8 +3,12 @@ package com.gestiondepedidos.sistemadegestiondepedidos.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +31,15 @@ public class TransportistaController {
     public List<Transportista> getAllTransportistas() {
         return transportistaService.getAllTransportistas();
     }
+
+    @PostMapping("/crear")
+    public ResponseEntity<Transportista> crearTransportista(@RequestBody Transportista transportista) {
+    try {
+        Transportista nuevoTransportista = transportistaService.saveTransportista(transportista);
+        return new ResponseEntity<>(nuevoTransportista, HttpStatus.CREATED);
+    } catch (Exception e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+}
     
 }
